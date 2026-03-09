a5b4e4af7cb5ce3a753a919a2283864e1bfcec77
noureddine.legragui <noureddinelegragui@gmail.com>
2026-03-09 10:50:24 +0000

    Configure project

---
+++ ThmanyahTestApp/ThmanyahTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahTestAppApp.swift
+//  ThmanyahTestApp
+//
+//  Created by Nour-Eddine Legragui  on 9/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
