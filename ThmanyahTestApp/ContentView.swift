32090a01757ea109316e947dd214d71efa335105
noureddine.legragui <noureddinelegragui@gmail.com>
2026-03-09 10:50:24 +0000

    Initial Commit

---
+++ ThmanyahTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThmanyahTestApp
+//
+//  Created by Nour-Eddine Legragui  on 9/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
