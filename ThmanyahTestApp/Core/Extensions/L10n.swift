d62314b4bd2e9dd60b8e9950e796c3c85da7fd5e
noureddine.legragui <noureddinelegragui@gmail.com>
2026-03-10 10:50:41 +0000

    update localized strings

--- ThmanyahTestApp/Core/Extensions/L10n.swift
+++ ThmanyahTestApp/Core/Extensions/L10n.swift
@@ -9,16 +9,16 @@ import Foundation
 
 enum L10n {
     enum Tab {
-        static let home = NSLocalizedString("tab.home", comment: "Home tab title")
-        static let search = NSLocalizedString("tab.search", comment: "Search tab title")
-        static let community = NSLocalizedString("tab.community", comment: "Community tab title")
-        static let library = NSLocalizedString("tab.library", comment: "Library tab title")
-        static let settings = NSLocalizedString("tab.setting", comment: "Settings tab title")
+        static let home = NSLocalizedString("tab.home", value: "الرئيسية", comment: "Home tab title")
+        static let search = NSLocalizedString("tab.search", value: "بحث", comment: "Search tab title")
+        static let community = NSLocalizedString("tab.community", value: "المجتمع", comment: "Community tab title")
+        static let library = NSLocalizedString("tab.library", value: "مكتبتي", comment: "Library tab title")
+        static let settings = NSLocalizedString("tab.setting", value: "الإعدادات", comment: "Settings tab title")
     }
 
     enum Home {
-        static let eveningGreeting = NSLocalizedString("home.greeting.evening", comment: "Evening greeting")
-        static let username = NSLocalizedString("home.greeting.username", comment: "Current user name in greeting")
-        static let filterAll = NSLocalizedString("home.filter.all", comment: "All content filter title")
+        static let eveningGreeting = NSLocalizedString("home.greeting.evening", value: "مساء الخير", comment: "Evening greeting")
+        static let username = NSLocalizedString("home.greeting.username", value: "عبدالرحمن", comment: "Current user name in greeting")
+        static let filterAll = NSLocalizedString("home.filter.all", value: "الكل", comment: "All content filter title")
     }
 }
